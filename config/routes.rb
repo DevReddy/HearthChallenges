169fbdcd4db92fafd0069326a919b33f6bc0f4fd
Ryan Eddy <ryaneddydev@gmail.com>
2015-05-04 17:10:36 -0400

    Fix controller routing, clean config/routes up a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'auth/reddit' => 'sessions/login'
-
-  get 'challenges/all'
-
   root 'static#index'
   get 'static/index'
+
+  get 'login/reddit' => 'sessions#login'
+  get 'auth/reddit' => 'sessions#auth'
+
+  get 'challenges/all'
 
 end
