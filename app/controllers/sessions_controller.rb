85078951c69e1a0b3d56081b01c36a6dd11254e3
Ryan Eddy <ryaneddydev@gmail.com>
2015-05-04 19:58:36 -0400

    Change query in post request to JSON

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,20 +11,20 @@ class SessionsController < ApplicationController
   	else
   		if params['state'] === session['state']
   			response = HTTParty.post("https://www.reddit.com/api/v1/access_token",
-  				:basic_auth => {
+  				{:basic_auth => {
   					:username => ENV['CLIENT_ID'],
   					:password => ENV['CLIENT_SECRET']
-  					},
-  				:body => {
+  					}},
+  				{:body => {
   					:grant_type => "authorization_code",
   					:code => params['code'],
   					:redirect_uri => "https://hearth-challenges-staging.herokuapp.com/auth/reddit"
-  					})
-  			reset_session
-  			session['username'] = HTTParty.get('https://oauth.reddit.com/api/v1/me',
-  				:headers => {
-  					"Authorization" => "bearer #{response.access_token}"
-  					})
+  					}})
+  			# reset_session
+  			# session['username'] = HTTParty.get('https://oauth.reddit.com/api/v1/me',
+  			# 	:headers => {
+  			# 		"Authorization" => "bearer #{response.access_token}"
+  			# 		})
   			redirect_to '/'
   		else
   			session['error'] = 'Returned state does not match.'
