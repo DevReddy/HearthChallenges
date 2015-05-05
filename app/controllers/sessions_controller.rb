d4999b546f38a4e0251d8ef5940eb075feaf0e8a
Ryan Eddy <ryaneddydev@gmail.com>
2015-05-04 20:56:00 -0400

    Add bad way to test JSON return values to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,8 +21,9 @@ class SessionsController < ApplicationController
   					:redirect_uri => "https://hearth-challenges-staging.herokuapp.com/auth/reddit"
   					}})
   			reset_session
-  			session['username'] = HTTParty.get('https://oauth.reddit.com/api/v1/me',
-  				headers: {"Authorization" => "bearer #{response[:access_token]}"})
+  			session['error'] = response[:access_token]
+  			# session['username'] = HTTParty.get('https://oauth.reddit.com/api/v1/me',
+  			# 	headers: {"Authorization" => "bearer #{response[:access_token]}"})
   			redirect_to '/'
   		else
   			session['error'] = 'Returned state does not match.'
