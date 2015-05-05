c956f8554c95bb6f2f98fb3a0215f2a33f608c3c
Ryan Eddy <ryaneddydev@gmail.com>
2015-05-05 09:47:14 -0400

    Validate Uniqueness of Challenge text

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,5 +1,5 @@
 class Challenge < ActiveRecord::Base
 	validates :author, presence: true
 	validates :rel_class, presence: true
-	validates :text, presence: true
+	validates :text, presence: true, uniqueness: true
 end
