450ec4af916dbe965296fefa34658f27eaaa4188
Ryan Eddy <ryaneddydev@gmail.com>
2015-05-05 08:51:19 -0400

    Add form to create Challenge if logged in

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -4,4 +4,11 @@ class ChallengesController < ApplicationController
   		format.json {render json: Challenge.all}
   	end
   end
+
+  def create
+  	challenge = Challenge.new(author: "/u/#{session['username']}", text: params['text'], rel_class: params['rel_class'])
+  	flash['error'] = challenge.errors if !challenge.save
+
+  	redirect_to '/'
+  end
 end
